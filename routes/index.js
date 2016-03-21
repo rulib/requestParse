f8ea643f471ad8b1d4c2f4cd06ea265bd15cbc51
Will D <wdemilio@gmail.com>
2016-03-21 15:48:48 +0000

    User-agent parsing implemented

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/api/whoami/', function(req, res, next) {
+    //ternary to handle weird browsers like lynx
+  var userAgentSoftware = (req.headers["user-agent"].split(/[(]|[)]/)[1] ? req.headers["user-agent"].split(/[(]|[)]/)[1] : req.headers["user-agent"]);
+  res.json({software:userAgentSoftware});
+  
 });
 
 module.exports = router;
